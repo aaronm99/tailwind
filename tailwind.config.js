9d7dfd0a7cf50017d4cf2a1fe0d4b2a4b2ef3527
Aaron Mistry <aaron.rohin@hotmail.co.uk>
2021-12-05 00:39:07 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        custompurple: "#1B263A",
+        customblue: "#134AE9",
+        gradientblue: "#C2DEF8",
+        gradientteal: "#D6F4F8",
+        gradientlight: "#DCEFF9",
+        backgroundblue: "#EDF4F6",
+        boxcolor: "#F9FBFB",
+      },
+      fontFamily: {
+        "space-grotesk": ["Space Grotesk"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
